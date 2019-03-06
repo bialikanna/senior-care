3c094eef7a7a779de9af40f17c815c3e8cb62f69
bialikanna <bialikanna@gmail.com>
2019-03-06 08:22:35 +0100

    fresh version

--- src/components/Header.js
+++ src/components/Header.js
@@ -24,7 +24,7 @@ export class Header extends React.Component {
           <div class="wrapper">
             <div class="logo">
               <a href="#">
-                <img src={logoSeniorCare} />{" "}
+                <img src={logoSeniorCare} />
               </a>
             </div>
             <nav>
